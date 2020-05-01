fafb5178ab35a1513040722044ff625323b0d2b2
Luis Gamarra Jimenez <luis.gamarra26@gmail.com>
2020-04-30 21:44:05 -0700

    Using the zmb spotify oauth package

--- src/main.go
+++ src/main.go
@@ -1,31 +1,71 @@
+// This example demonstrates how to authenticate with Spotify using the authorization code flow.
+// In order to run this example yourself, you'll need to:
+//
+//  1. Register an application at: https://developer.spotify.com/my-applications/
+//       - Use "http://localhost:8080/callback" as the redirect URI
+//  2. Set the SPOTIFY_ID environment variable to the client ID you got in step 1.
+//  3. Set the SPOTIFY_SECRET environment variable to the client secret from step 1.
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 	"net/http"
-	"spotify"
-	"spotify/authenticate"
+
+	"github.com/zmb3/spotify"
 )
 
-const redirectUrl = "http://localhost:8080/callback"
+// redirectURI is the OAuth redirect URI for the application.
+// You must register an application at Spotify's developer portal
+// and enter this value.
+const redirectURI = "http://localhost:8080/callback"
 
 var (
-	auth = authenticate.NewAuthenticator(redirectUrl, "user-library-read")
-	userChannel = make(chan spotify.Client)
-	state = "placeholder"
+	auth  = spotify.NewAuthenticator(redirectURI, spotify.ScopeUserReadPrivate)
+	ch    = make(chan *spotify.Client)
+	state = "abc123"
 )
 
 func main() {
-	http.HandleFunc("/callback", callbackHandler)
+	// first start an HTTP server
+	http.HandleFunc("/callback", completeAuth)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Got request for:", r.URL.String())
+	})
+	go http.ListenAndServe(":8080", nil)
 
-	fmt.Println(auth.AuthURL(state))
+	url := auth.AuthURL(state)
+	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
 
-	//go http.ListenAndServe(":8080", nil)
+	// wait for auth to complete
+	client := <-ch
 
-
-
+	// use the client to make calls that require authorization
+	user, err := client.CurrentUser()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("You are logged in as:", user.ID)
 }
 
-func callbackHandler(w http.ResponseWriter, r *http.Request) {
+func completeAuth(w http.ResponseWriter, r *http.Request) {
+	tok, err := auth.Token(state, r)
+	if err != nil {
+		http.Error(w, "Couldn't get token", http.StatusForbidden)
+		buf := new(bytes.Buffer)
+		////buf.ReadFrom(err.Body)
+		newStr := buf.String()
 
+		fmt.Printf(newStr)
+		log.Fatal(err)
+	}
+	if st := r.FormValue("state"); st != state {
+		http.NotFound(w, r)
+		log.Fatalf("State mismatch: %s != %s\n", st, state)
+	}
+	// use the token to get an authenticated client
+	client := auth.NewClient(tok)
+	fmt.Fprintf(w, "Login Completed!")
+	ch <- &client
 }
